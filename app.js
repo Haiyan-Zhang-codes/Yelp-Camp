394d7c8ab3d745fef3f57242a66e90f6375c291f
Haiyan Zhang <haiyan.zhang1982@gmail.com>
2023-11-17 22:31:02 -0500

    errors

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', ()=>{
+    console.log('Database connected')
+})
+
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
+
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.listen(3000,()=>{
+    console.log('Serving on port 3000')
+})
