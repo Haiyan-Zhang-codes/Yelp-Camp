718a3b0b2357a34cf08a546bc22bdfaaa4eebb36
Haiyan Zhang <haiyan.zhang1982@gmail.com>
2023-11-23 19:45:40 -0500

    try extension--havent finished the styling

--- seeds/index.js
+++ seeds/index.js
@@ -17,10 +17,14 @@ const titleSample = array => array[Math.floor(Math.random()*array.length)];
 const seedDB = async ()=>{
     await Campground.deleteMany();
     for (let i=0; i<50; i++){
-        const random1000 = Math.floor(Math.random()*1000)
+        const random1000 = Math.floor(Math.random()*1000);
+        const randomPrice = Math.floor(Math.random()*30)+20;
         const camp = new Campground({
             location: `${cities[random1000].city}-${cities[random1000].state}`,
-            title: `${titleSample(descriptors)} ${titleSample(places)}`
+            title: `${titleSample(descriptors)} ${titleSample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis delectus maiores sequi quisquam nisi corporis labore iure nobis nostrum architecto dolorum itaque, unde assumenda cum, deleniti praesentium culpa? Sequi, voluptates!',
+            price: randomPrice
         
         })
         await camp.save();
