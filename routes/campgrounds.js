47a7e8645ff09aad964646d48002eeb4fce7ad64
Haiyan Zhang <haiyan.zhang1982@gmail.com>
2023-12-27 11:21:53 -0500

    ddd

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const {isLoggedIn,isAuthor,validateCampground} = require('../middleware.js');
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const {storage} = require('../cloudinary')
+const upload = multer({storage})
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn,validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn,validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req,res)=>{
+        console.log(req.body, req.files);
+        res.send('IT WORKED')
+    })
 
 router.get('/new', isLoggedIn,campgrounds.renderNewForm);
 
